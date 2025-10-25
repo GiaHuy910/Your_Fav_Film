e7db40202124f715ce0c9a0d2f6f9b52edcf9055
GiaHuy910 <nguyenbui.09101999@gmail.com>
2025-10-25 16:10:06 +0700

    giai doan 1 da xong

--- Main.py
+++ Main.py
@@ -1,1 +1,36 @@
-print("huy sua lan 1")
+import pandas as pd
+import sys
+import ast
+sys.stdout.reconfigure(encoding='utf-8')
+
+# Đọc dữ liệu
+movies = pd.read_csv('tmdb_5000_movies.csv')
+credits = pd.read_csv('tmdb_5000_credits.csv')
+
+mcredit=pd.merge(movies,credits,left_on="id",right_on="movie_id")
+
+def Convert(obj):
+    try:
+        L = []
+        for i in ast.literal_eval(obj):
+            L.append(i['name'])
+        return L
+    except:
+        return []
+def get_director(obj):
+    try:
+        for i in ast.literal_eval(obj):
+            if i['job'] == 'Director':
+                return i['name']
+        return ""
+    except:
+        return ""
+    
+mcredit['genres']=mcredit['genres'].apply(Convert)
+mcredit['keywords']=mcredit['keywords'].apply(Convert)
+mcredit['cast']=mcredit['cast'].apply(Convert)
+mcredit['crew']=mcredit['crew'].apply(Convert)
+mcredit['overview']=mcredit['overview'].fillna("").apply(lambda x: x.split())
+
+mcredit['title'] = mcredit['original_title']
+print(mcredit[['title','genres', 'cast', 'crew', 'keywords','overview']].head())
